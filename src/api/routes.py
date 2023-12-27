70ac6c108b9d2dd637c6c8f29509535d2ec4bdfb
Isaac F <114119376+boxmancoder@users.noreply.github.com>
2023-12-27 01:30:09 +0000

    worked on api section

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,19 +4,46 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager, create_access_token
 from flask_cors import CORS
 
 api = Blueprint('api', __name__)
+CORS(api, resources={r"/api/*": {"origins": "*", "methods": ["GET", "POST", "PUT", "DELETE"]}})
 
 # Allow CORS requests to this API
 CORS(api)
 
+@api.route('/users', methods=['GET', 'POST'])
+def manage_users():
+    if request.method == 'GET':
+        users = User.query.all()
+        return jsonify([user.username for user in users])
+    elif request.method == 'POST':
+        data = request.json
+        user = User(username=data['username'], email=data['email'], 
+                        active=data['active'], password=generate_password_hash(data['password']))
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({'message': 'Usuario creado exitosamente'}), 201
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        users = User.query.all()
+        return jsonify([user.username for user in users])
+    elif request.method == 'POST':
+        data = request.json
+        print(data)
+        email = data.get('email')
+        password = data.get('password')
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    if not email or not password:
+        return jsonify({'message': 'Email and password are required'}), 400
+    user = User.query.filter_by(email=email).first()
 
-    return jsonify(response_body), 200
+    if not user or not check_password_hash(user.password, password):
+        return jsonify({'message': 'Invalid email or password'}), 401
+    
+    token = create_access_token(identity={'email': user.email})
+    return jsonify({'token': token, 'message': 'Login successful'}), 200
